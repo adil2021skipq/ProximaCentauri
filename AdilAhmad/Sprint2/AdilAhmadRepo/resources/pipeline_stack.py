8fcf09b57cbc658b42b5f381c677080401ff6ba3
Ubuntu <ubuntu@ip-172-31-41-129.us-east-2.compute.internal>
2021-12-24 08:43:43 +0000

    part 10

--- AdilAhmad/Sprint2/AdilAhmadRepo/resources/pipeline_stack.py
+++ AdilAhmad/Sprint2/AdilAhmadRepo/resources/pipeline_stack.py
@@ -38,7 +38,8 @@ class MyPipelineStack(core.Stack):
         role=aws_iam.Role(self,"pipeline-role",
         assumed_by=aws_iam.CompositePrincipal(
             aws_iam.ServicePrincipal("lambda.amazonaws.com"),
-            aws_iam.ServicePrincipal("sns.amazonaws.com")
+            aws_iam.ServicePrincipal("sns.amazonaws.com"),
+            aws_iam.ServicePrincipal('codebuild.amazonaws.com')
             ),
         managed_policies=[
             aws_iam.ManagedPolicy.from_aws_managed_policy_name('service-role/AWSLambdaBasicExecutionRole'),
