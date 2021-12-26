a41afb3cbfd3db15f48ed9858cc435bb19629edc
Ubuntu <ubuntu@ip-172-31-41-129.us-east-2.compute.internal>
2021-12-26 17:34:02 +0000

    take13

--- AdilAhmad/Sprint2/AdilAhmadRepo/resources/DynamoDB_lambda.py
+++ AdilAhmad/Sprint2/AdilAhmadRepo/resources/DynamoDB_lambda.py
@@ -1,13 +1,13 @@
-# import boto3
+import boto3
 
 
-# def lambda_handler(event, context):
-#     db = boto3.client("dynamodb")
-#     message = event['Records'][0]['Sns']['MessageId']
-#     time = event['Records'][0]['Sns']['Timestamp']
-#     alarmName = event['Records'][0]['Sns']['Subject']
-#     db.put_item(TableName="AdilAhmadAlarmTable", Item={
-#         'id':{'S':message},
-#         'createdDate':{'S':time},
-#         'name':{'S':alarmName}
-#     })
+def lambda_handler(event, context):
+    db = boto3.client("dynamodb")
+    message = event['Records'][0]['Sns']['MessageId']
+    time = event['Records'][0]['Sns']['Timestamp']
+    alarmName = event['Records'][0]['Sns']['Subject']
+    db.put_item(TableName="AdilAhmadAlarmTable", Item={
+        'id':{'S':message},
+        'createdDate':{'S':time},
+        'name':{'S':alarmName}
+    })
