59639092f4aa0c6bd5127bfb4f96ee120a28bbec
Ubuntu <ubuntu@ip-172-31-41-129.us-east-2.compute.internal>
2021-12-27 16:26:38 +0000

    a8

--- AdilAhmad/Sprint2/AdilAhmadRepo/resources/DynamoDB_lambda.py
+++ AdilAhmad/Sprint2/AdilAhmadRepo/resources/DynamoDB_lambda.py
@@ -1,17 +1,17 @@
-import boto3
+# import boto3
 
 
-def lambda_handler(event, context):
-    db = boto3.client("dynamodb")
-    message = event['Records'][0]['Sns']['MessageId']
-    time = event['Records'][0]['Sns']['Timestamp']
-    alarmName = event['Records'][0]['Sns']['Subject']
-    if message[0]=="B":
-        table_name="Beta-adil-stack-TableCD117FA1-9T5UV5HM0L7X"
-    else:
-        table_name="Prod-adil-stack-TableCD117FA1-584GC4KCY32C"
-    db.put_item(TableName=table_name, Item={
-        'id':{'S':message},
-        'createdDate':{'S':time},
-        'name':{'S':alarmName}
-    })
+# def lambda_handler(event, context):
+#     db = boto3.client("dynamodb")
+#     message = event['Records'][0]['Sns']['MessageId']
+#     time = event['Records'][0]['Sns']['Timestamp']
+#     alarmName = event['Records'][0]['Sns']['Subject']
+#     if message[0]=="B":
+#         table_name="Beta-adil-stack-TableCD117FA1-9T5UV5HM0L7X"
+#     else:
+#         table_name="Prod-adil-stack-TableCD117FA1-584GC4KCY32C"
+#     db.put_item(TableName=table_name, Item={
+#         'id':{'S':message},
+#         'createdDate':{'S':time},
+#         'name':{'S':alarmName}
+#     })
